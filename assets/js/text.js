11c474a3ec6f8acedda20163c6080b3c4b180cbd
bog-danius <jenesius1998@gmail.com>
2022-11-07 01:00:58 +0300

    печатная машинка

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,22 +1,32 @@
-let text= [
-    '&gt welcome @Burdin',
-    '&gt interface show header',
-    '&gt interface show social_network',
-    '&gt user load infromation',
-    '&gt interface show user-data',
-    '&gt store reset'
-]
-function typeText(){
-let line= 0;
-let count = 0;
-let out = '';
-let text1 = document.querySelector('.out');
-    function typeLine(){
-let interval = setTimeout( function(){
-    out += text[line][count];
-    text1.innerHTML=out;
-    } ,200)
-    }
-    typeLine();
+let text = [
+  "&gt welcome @Burdin\n",
+  "&gt interface show header\n",
+  "&gt interface show social_network\n",
+  "&gt user load infromation\n",
+  "&gt interface show user-data\n",
+  "&gt store reset",
+];
+function typeText() {
+  let line = 0;
+  let count = 0;
+  let out = "";
+  let text1 = document.querySelector(".info");
+  function typeLine() {
+    let interval = setTimeout(function () {
+      out += text[line][count];
+      text1.innerHTML = out + "|";
+      count++;
+      if (count >= text[line].length) {
+        count = 0;
+        line++;
+        if (line == text.length) {
+          clearTimeout(interval);
+          return true;
+        }
+      }
+      typeLine();
+    }, 100);
+  }
+  typeLine();
 }
 typeText();
